276da3be73a8a98ac0cb495e6619995b1bc41591
Matthew Marion <matthewjmarion29@gmail.com>
2017-10-07 21:33:53 -0400

    Navbar/store button work

---
+++ public/javascripts/cssutils.js
@@ -0,0 +1,6 @@
+$('.nav-store').hover(function() {
+    $(this).css('background-color', '#ff1915');
+    $(this).css('color', 'white');
+}, function() {
+    $(this).css('background-color', '#ff7f7d');
+});
