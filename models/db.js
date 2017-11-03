f6e543735d772971a7b285de1a4954f11ab596ed
matthewmarion <matthewjmarion29@gmail.com>
2017-11-02 21:56:18 -0500

    Mongo disconnection events

--- models/db.js
+++ models/db.js
@@ -1,7 +1,18 @@
 let mongoose = require('mongoose');
-
+let gracefulShutdown;
 let dbURI = 'mongodb://localhost/CaliburnWeb';
 mongoose.connect(dbURI);
+
+let readLine = require('readline');
+if (process.platform == 'win32') {
+    let rl = readLine.createInterface ({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on('SIGINT', function() {
+        process.emit('SIGINT');
+    });
+}
 
 mongoose.connection.on('connected', function() {
     console.log('Mongoose connected to ' + dbURI);
@@ -13,4 +24,29 @@ mongoose.connection.on('error', function(err) {
 
 mongoose.connection.on('disconnected', function() {
     console.log('Mongoose disconnected');
-}); 
+});
+
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.once('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app shutdown', function() {
+        process.exit(0);
+    });
+});
