13f34b7607cdd388395cc816e58306e1274a50f1
matthewmarion <matthewjmarion29@gmail.com>
2017-11-11 23:36:10 -0600

    design changes

--- app.js
+++ app.js
@@ -20,11 +20,9 @@ redditService.submissions.on('submission', function(submission) {
 	redditService.filterPostRequirements(submission);
 });
 
-// view engine setup
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -35,20 +33,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/api', routesApi);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
 	var err = new Error('Not Found');
 	err.status = 404;
 	next(err);
 });
 
-// error handler
 app.use(function(err, req, res) {
-	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-	// render the error page
 	res.status(err.status || 500);
 	res.render('error');
 });
