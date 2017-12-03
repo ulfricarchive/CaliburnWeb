255690fde6e539bd58c8696dfd5c8d9139b909de
matthewmarion <matthewjmarion29@gmail.com>
2017-12-03 12:41:42 -0600

    Fixed navbar

--- public/javascripts/cssutils.js
+++
@@ -1,6 +0,0 @@
-$('.nav-store').hover(function() {
-    $(this).css('background-color', '#ff1915');
-    $(this).css('color', 'white');
-}, function() {
-    $(this).css('background-color', '#ff7f7d');
-});
